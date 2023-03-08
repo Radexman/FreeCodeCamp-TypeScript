234ece934649b0a4e73300d653401aa9079cfe1b
Radosław Siek <Borderlandsmaniak@gmail.com>
2023-03-08 16:13:08 +0100

    08.03

---
+++ myFunctions.ts
@@ -0,0 +1,20 @@
+function addTwo (num: number) {
+    // return num + 2;
+    return 'hello';
+}
+
+function getUpper(val: string) {
+    return val.toUpperCase();
+}
+
+function signUpUser(name: string, email: string, isPaid: boolean) {
+
+}
+
+const loginUser = (name: string, email: string, isPaid: boolean = false) => {
+
+}
+let myValue = addTwo(5);
+getUpper('radek');
+signUpUser('emilia', "emi@wp.pl", true);
+loginUser('h', 'h@h.com');
--- variablesMe.ts
+++ variablesMe.ts
@@ -1,6 +1,21 @@
-let greetings: string = 'Hello Emilia';
-let myNum = 6;
+let greetings = 'Hello Emilia';
+
 greetings.toLowerCase();
 console.log(greetings);
+
+// number
+let userId = 224466.54;
+userId.toFixed();
+
+// boolean
+let isLoggedIn = false;
+
+let hero: string;
+
+function getHero() {
+    return 'thor'
+}
+
+hero = getHero();
 
 export {};
