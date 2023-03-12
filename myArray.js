8d4acaf68f5c966cd76bdcc7cf855a68a655bb88
Radosław Siek <Borderlandsmaniak@gmail.com>
2023-03-12 15:20:21 +0100

    Array Type
    12.03

---
+++ myArray.js
@@ -0,0 +1,7 @@
+var superHeroes = [];
+// const heroPower: number[] = [];
+var heroPower = [];
+var allUsers = [];
+superHeroes.push('Batman');
+heroPower.push(2);
+allUsers.push({ name: 'Emilia', isActive: true });
---
+++ myArray.ts
@@ -0,0 +1,14 @@
+const superHeroes: string[] = [];
+// const heroPower: number[] = [];
+const heroPower: Array<number> = [];
+
+type User = {
+    name: string,
+    isActive: boolean
+}
+
+const allUsers: User[] = [];
+
+superHeroes.push('Batman');
+heroPower.push(2);
+allUsers.push({name: 'Emilia', isActive: true});
