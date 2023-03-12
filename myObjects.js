2298f4b18c5e7c9c6f1bb6128a083938568bbb9a
Radosław Siek <Borderlandsmaniak@gmail.com>
2023-03-12 14:48:13 +0100

    Type Alias
    12.03

---
+++ myObjects.js
@@ -0,0 +1,13 @@
+"use strict";
+// const User = {
+//     name: 'radek',
+//     email: 'radeg69@wp.pl',
+//     isActive: true
+// }
+exports.__esModule = true;
+function createUser(user) {
+    return {
+        name: '', email: '', isActive: true
+    };
+}
+createUser({ name: '', email: '', isActive: true });
--- myObjects.ts
+++ myObjects.ts
@@ -1,17 +1,31 @@
-const User = {
-    name: 'radek',
-    email: 'radeg69@wp.pl',
-    isActive: true
+// const User = {
+//     name: 'radek',
+//     email: 'radeg69@wp.pl',
+//     isActive: true
+// }
+
+// function createUser({name: string, isPaid: boolean}) {
+
+// }
+
+// createUser({name: 'radek', isPaid: false});
+
+// function createCourse():{name: string, price: number}{
+//     return {name: 'reactjs', price: 399};
+// }
+
+type User = {
+    name: string,
+    email: string,
+    isActive: boolean,
 }
 
-function createUser({name: string, isPaid: boolean}) {
-
+function createUser(user: User): User {
+    return {
+        name: '', email: '', isActive: true
+    }
 }
 
-createUser({name: 'radek', isPaid: false});
-
-function createCourse():{name: string, price: number}{
-    return {name: 'reactjs', price: 399};
-}
+createUser({name: '', email: '', isActive: true});
 
 export {};
